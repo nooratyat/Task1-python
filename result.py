8caf56bfceccab82343d21e4e9cd7798645a0563
noor atyat <nooratyat92@gmail.com>
2017-05-30 03:47:32 +0300

    make obj loader

--- result.py
+++ result.py
@@ -1,29 +1,28 @@
 # open files for reading 
-# f=open('object_B.obj','r')
-# print(f.name)
-# f.close() 
-# with open('object_A.obj','r') as f:
-# 	f_inside=f.read()
-# 	print(f_inside)
+
 from mesh import obj 
+from OpenGL.GL import *
+
 
 first = obj.Obj("object_A.obj") 
 second= obj.Obj("object_B.obj") 
 class OBJ:
 	"""docstring for OBJ"""
-	def __init__(self, object_A):
-		# super(OBJ, self).__init__()
+	def __init__(self, first,swapyz):
+		
 		self.Ver = []
 		self.fa=[]
+		
+        material = None
 
 		for line in open('object_A','r'):
 			values=line.split()
 			if not values:continue
-			if values[0]=='v'
+			if values[0]=='v':
 			v=map(float,values[1:4])
 			if swapyz:
 				v=v[0],v[1],v[2]
 				self.Ver.append(v)
-            if values[0]=='f'
+            elif values[0]=='f':
             f=map(float,values[1,4])
                 self.fa.append(f)
--- test.py
+++ test.py
@@ -6,8 +6,43 @@
 # with open('object_A.obj','r') as f:
 # 	f_inside=f.read()
 # 	print(f_inside)
-import pywavefront
-    from pyglet.gl import *
+# ////////////////////////////////////////
+# import pywavefront
+#     from pyglet.gl import *
 
-meshes = pywavefront.Wavefront('object_A.obj')
-meshes.draw()
+# meshes = pywavefront.Wavefront('object_A.obj')
+# meshes.draw()
+# ///////////////////////
+import numpy as np
+
+class ObjLoader:
+	
+	def __init__(self, arg):
+	
+		self.vert = []
+		self.face = []
+		self.model=[]
+	def load-model(self,file):
+			for line in open(file,'r'):
+				if line.startwith('#'):continue
+				values=line.split()
+			    if not values:continue
+
+			    if values[0] == 'v':
+			    	self.vert.append(values[1:4])
+			    if values[0] == 'f':
+			        self.face.append(values[1:4])	
+
+
+                    self.model = np.array(self.model, dtype='float32')
+
+
+
+                    obj=ObjLoader()
+                    obj.load-model("object_A.obj")
+
+
+
+
+
+
